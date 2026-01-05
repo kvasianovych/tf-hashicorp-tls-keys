aee7017ecaadd653448121825a761f80598284f1
Kostiantyn Vasianovych <k.vasianovych@pm.me>
2026-01-05 19:14:11 +0200

    Formatting

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "private_key_pem" {
   sensitive = true
-  value = tls_private_key.this.private_key_pem
+  value     = tls_private_key.this.private_key_pem
 }
 
 output "public_key_openssh" {
   sensitive = true
-  value = tls_private_key.this.public_key_openssh
+  value     = tls_private_key.this.public_key_openssh
 }
